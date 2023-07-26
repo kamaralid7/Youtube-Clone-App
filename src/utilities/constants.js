be95c1b3453c657f5b0fc3820760a985d911fb9a
kamaralid7 <126551638+kamaralid7@users.noreply.github.com>
2023-07-26 10:15:34 +0530

    Update constants.js

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -5,5 +5,5 @@
 //const GOOGLE_API_KEY = "AIzaSyDaYOvtKdmH5SKCKnQZ4uBzuJKGmvzu0jE";
 
 export const YOUTUBE_API =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=GB&key=" +
   GOOGLE_API_KEY;
